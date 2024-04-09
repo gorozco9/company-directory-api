f515687ce76ca8b9b725c1fa100e7cb89b53203a
gorozco9 <gorozco9@stu.southtexascollege.edu>
2024-04-09 00:00:32 -0500

    connect to api is working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,20 @@
 <script setup>
-//import { ref } from 'vue'
+import { onMounted } from 'vue'
 //const title = ref('Hello There ✋')
 import MainCard from '@/components/MainCard.vue';
 import MainPagination from '@/components/MainPagination.vue';
+import useAPI from '@/composables/useAPI';
+
+const { getEmployees, loading } = useAPI()
+
+onMounted(async () => {
+  await getEmployees()
+})
+
 </script>
 
 <template>
-  <MainCard></MainCard>
-  <MainPagination></MainPagination>
+  <p v-if="loading">LOADING...></p>
+   <MainCard></MainCard>
+   <MainPagination></MainPagination>
 </template>
