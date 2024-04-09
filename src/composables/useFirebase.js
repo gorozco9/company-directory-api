bbfdab805380ea6e18a85e8734ddb8b66468eefd
gorozco9 <137967565+gorozco9@users.noreply.github.com>
2024-04-08 22:31:31 -0500

    Initial commit

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBRL_Uz0NwW240yzYCr5q4EHGcpIuUwiBI",
+  authDomain: "company-directory-b9427.firebaseapp.com",
+  projectId: "company-directory-b9427",
+  storageBucket: "company-directory-b9427.appspot.com",
+  messagingSenderId: "303192893141",
+  appId: "1:303192893141:web:d3dd9e052712d692cb245c"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+//import { ref } from 'vue'
+//const title = ref('Hello There ✋')
+import MainCard from '@/components/MainCard.vue';
+import MainPagination from '@/components/MainPagination.vue';
+</script>
+
+<template>
+  <MainCard></MainCard>
+  <MainPagination></MainPagination>
+</template>
